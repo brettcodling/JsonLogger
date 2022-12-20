73c4b6ce4dca739a84c3aef329e3f0dad7d38d7d
Brett Codling <brett.codling@toolstation.com>
2022-12-20 13:56:36 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brettcodling/JsonLogger
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,99 @@
+package jsonlogger
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type JsonLog struct {
+	Message  string      `json:"message"`
+	Context  interface{} `json:"context"`
+	Type     string      `json:"type"`
+	Severity string      `json:"severity"`
+}
+
+// log will log a json message
+// If the message cannot be marshalled the json error will be logged
+func (jLog JsonLog) Log() {
+	// Set default parameters
+	if jLog.Type == "" {
+		jLog.Type = "log"
+	}
+	if jLog.Severity == "" {
+		jLog.Severity = "INFO"
+	}
+	encodedLog, err := json.Marshal(jLog)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(string(encodedLog))
+}
+
+// Info will log a json message with a severity of INFO
+func Info(msg string, context ...interface{}) {
+	var c interface{}
+	switch {
+	case len(context) == 1:
+		c = context[0]
+	case len(context) > 1:
+		c = context
+	}
+	JsonLog{
+		Message:  msg,
+		Context:  c,
+		Severity: "INFO",
+		Type:     "log",
+	}.Log()
+}
+
+// Error will log a json message with a severity of ERROR
+func Error(msg string, context ...interface{}) {
+	var c interface{}
+	switch {
+	case len(context) == 1:
+		c = context[0]
+	case len(context) > 1:
+		c = context
+	}
+	JsonLog{
+		Message:  msg,
+		Context:  c,
+		Severity: "ERROR",
+		Type:     "log",
+	}.Log()
+}
+
+// Warn will log a json message with a severity of WARNING
+func Warn(msg string, context ...interface{}) {
+	var c interface{}
+	switch {
+	case len(context) == 1:
+		c = context[0]
+	case len(context) > 1:
+		c = context
+	}
+	JsonLog{
+		Message:  msg,
+		Context:  c,
+		Severity: "WARNING",
+		Type:     "log",
+	}.Log()
+}
+
+// Debug will log a json message with a severity of DEBUG
+func Debug(msg string, context ...interface{}) {
+	var c interface{}
+	switch {
+	case len(context) == 1:
+		c = context[0]
+	case len(context) > 1:
+		c = context
+	}
+	JsonLog{
+		Message:  msg,
+		Context:  c,
+		Severity: "DEBUG",
+		Type:     "log",
+	}.Log()
+}
